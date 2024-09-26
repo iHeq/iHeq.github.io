56c7ada69a91526ca1a713df5648846966a252d4
ui <unistlogo@gmail.com>
2024-09-26 09:14:24 +0200

    website

---
+++ script.js
@@ -0,0 +1,29 @@
+// script.js
+
+let audio = new Audio('audio.mp3');
+audio.loop = true;
+
+document.addEventListener('click', () => {
+    document.querySelector('.enter').style.opacity = 0;
+    document.querySelector('.content').style.opacity = 1;
+    audio.play();
+});
+
+// script.js
+let texts = ['musician.', 'developer.', 'student.'];
+let index = 0;
+let textElement = document.getElementById('text');
+
+function changeText() {
+    textElement.classList.remove('show');
+    setTimeout(() => {
+        textElement.textContent = texts[index];
+        textElement.classList.add('show');
+        index = (index + 1) % texts.length;
+    }, 500);
+}
+
+setInterval(changeText, 2000);
+
+
+
